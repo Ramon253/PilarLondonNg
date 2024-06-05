aa796551246221e0f7574209eb744c90eeaec439
Ramon253 <jgalazc253@g.educaand.es>
2024-06-05 13:20:54 +0200

    Tests

--- src/app/resources/file/file.component.ts
+++ src/app/resources/file/file.component.ts
@@ -1,56 +1,54 @@
-import { Component, SimpleChange, SimpleChanges, input, output, signal } from '@angular/core';
-import { FileR } from '../../models/properties/file';
-import { LoginService } from '../../login.service';
-import { LoadingWheelComponent } from '../../svg/loading-wheel/loading-wheel.component';
-import { FileService } from '../../services/resources/file.service';
+import {Component, input, output, signal} from '@angular/core';
+import {FileR} from '../../models/properties/file';
+import {LoginService} from '../../login.service';
+import {LoadingWheelComponent} from '../../svg/loading-wheel/loading-wheel.component';
+import {FileService} from '../../services/resources/file.service';
 import {environment} from "../../../environments/environment.development";
 
 
 @Component({
-  selector: 'app-file',
-  standalone: true,
-  imports: [LoadingWheelComponent],
-  templateUrl: './file.component.html',
-  styleUrl: './file.component.css'
+    selector: 'app-file',
+    standalone: true,
+    imports: [LoadingWheelComponent],
+    templateUrl: './file.component.html',
+    styleUrl: './file.component.css'
 })
 export class FileComponent {
-  file = input<FileR>()
-  color = input<string>('secondary')
-  showControls = input<boolean>(false)
-  hoverCloseButton = signal<boolean>(false)
-  delete = output<{ id: string, isMultimedia: boolean }>()
+    file = input<FileR>()
+    color = input<string>('secondary')
+    showControls = input<boolean>(false)
+    hoverCloseButton = signal<boolean>(false)
+    delete = output<{ id: string, isMultimedia: boolean }>()
 
     parent = input.required<string>()
-  isLoadingDelete = signal<boolean>(false)
+    isLoadingDelete = signal<boolean>(false)
 
-  constructor(
-    public loginSvc: LoginService,
-    public fileSvc: FileService
-  ) { }
-
-    download( ){
-      this.fileSvc.downLoadFile(environment.baseUrl + `api/${this.parent()}/file/${this.file()?.id}/download`).subscribe(
-          (blob: Blob) => {
-              const url = window.URL.createObjectURL(blob);
-              const a = document.createElement('a');
-              a.href = url;
-              a.download =  this.file()?.file_name ?? '';
-              document.body.appendChild(a);
-              a.click();
-              document.body.removeChild(a);
-              window.URL.revokeObjectURL(url);
-          },
-          error => console.error('Download failed', error)
-      );
+    constructor(
+        public loginSvc: LoginService,
+        public fileSvc: FileService
+    ) {
     }
-  deleteFile() {
-    this.fileSvc.destroyFile(this.parent(),this)
-  }
 
-  disableLink(event: Event) {
-    if (this.hoverCloseButton())
-      event.preventDefault()
-  }
+    download() {
+        this.fileSvc.downLoadFile(environment.baseUrl + `api/${this.parent()}/file/${this.file()?.id}/download`).subscribe(
+            (blob: Blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const newTab = window.open();
+                if (newTab !== null)
+                    newTab.location.href = url;
+            },
+            error => console.error('Download failed', error)
+        );
+    }
+
+    deleteFile() {
+        this.fileSvc.destroyFile(this.parent(), this)
+    }
+
+    disableLink(event: Event) {
+        if (this.hoverCloseButton())
+            event.preventDefault()
+    }
 
     protected readonly environment = environment;
 }
